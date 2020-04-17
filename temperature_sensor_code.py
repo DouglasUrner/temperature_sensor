8186aa37ec20faf973ddaf2986593d084278c4db
Douglas Urner <4495780+DouglasUrner@users.noreply.github.com>
2020-04-17 16:52:48 -0700

    Update temperature_sensor_code.py

--- temperature_sensor_code.py
+++ temperature_sensor_code.py
@@ -22,10 +22,10 @@ def read_temp():
         lines = read_temp_raw()
     equals_pos = lines[1].find('t=')
     if equals_pos != -1:
-        temp_string = lines[1][equals_pos+2:]
+        temp_raw = lines[1][equals_pos+2:]
         temp_c = float(temp_string) / 1000.0
         temp_f = temp_c * 9.0 / 5.0 + 32.0
-        return temp_c, temp_f
+        return temp_raw, temp_c, temp_f
 	
 while True:
 	print(read_temp())	
